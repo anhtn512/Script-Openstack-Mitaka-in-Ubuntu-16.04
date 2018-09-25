185699aafe05d8baf02b8c3326be7908fd171782
= <=>
2018-09-25 10:13:16 +0700

    update code

--- ctl-2-prepare.sh
+++ ctl-2-prepare.sh
@@ -62,9 +62,49 @@ echocolor "Configuring MYSQL"
 sleep 5
 
 mysql_server=/etc/mysql/mariadb.conf.d/50-server.cnf
-ops_edit $mysql_server mysqld bind-address 0.0.0.0
-ops_edit $mysql_server mysqld character-set-server utf8
-ops_edit $mysql_server mysqld collation-server utf8_general_ci
+test -f $mysql_server.orig || cp $mysql_server $mysql_server.orig
+rm $mysql_server
+touch $ifaces
+cat << EOF >> $mysql_server
+[server]
+
+[mysqld]
+
+user            = mysql
+pid-file        = /var/run/mysqld/mysqld.pid
+socket          = /var/run/mysqld/mysqld.sock
+port            = 3306
+basedir         = /usr
+datadir         = /var/lib/mysql
+tmpdir          = /tmp
+lc-messages-dir = /usr/share/mysql
+skip-external-locking
+
+bind-address            = 0.0.0.0
+
+key_buffer_size         = 16M
+max_allowed_packet      = 16M
+thread_stack            = 192K
+thread_cache_size       = 8
+myisam-recover         = BACKUP
+
+query_cache_limit       = 1M
+query_cache_size        = 16M
+
+log_error = /var/log/mysql/error.log
+expire_logs_days        = 10
+max_binlog_size   = 100M
+
+character-set-server  = utf8
+collation-server      = utf8_general_ci
+
+[embedded]
+
+[mariadb]
+
+[mariadb-10.0]
+
+EOF
 
 mysql_client=/etc/mysql/mariadb.conf.d/50-client.cnf
 ops_edit $mysql_client client default-character-set utf8
